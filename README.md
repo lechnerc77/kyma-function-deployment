1941b813f046c1c7d4f18b85f3686432b714b7f6
Christian Lechner <lechnerc77@users.noreply.github.com>
2021-01-31 11:11:24 +0100

    first try func from git

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample Repository for Kyma Function Deployment
+
+This repository contains sample code for the deployment of a Kyma function from a GitHub repository.
---
+++ function/handler.js
@@ -0,0 +1,21 @@
+const axios = require("axios")
+const qs = require("qs")
+
+module.exports = {
+    main: async function (event, context) {
+        console.log("------------------")
+
+        console.log("Values from Secret")
+        console.log("------------------")
+        console.log(process.env['CLIENT_ID'])
+        console.log(process.env['CLIENT_SECRET'])
+
+        console.log("------------------")
+
+        console.log("Values from ConfigMap")
+        console.log("------------------")
+        console.log(process.env['CONFIG'])
+        console.log(process.env['BASE_URL'])
+
+    }
+}
